87074c375237a8a7f2f4e3d5b2fc8d5a8854783f
Dan Debrunner <djd@debrunners.com>
2019-04-16 22:30:17 +0000

    Set port/ip

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ Object.assign=require('object-assign')
 app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'))
 
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+
 app.get('/', function (req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
